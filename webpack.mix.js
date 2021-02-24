44675c1fb7e4f66d6b0898ad459d758adb55928a
Michael <michael.alesiano@gmail.com>
2021-02-24 13:38:13 +0100

    First push

---
+++ webpack.mix.js
@@ -0,0 +1,13 @@
+// webpack.mix.js
+
+let mix = require('laravel-mix');
+
+mix
+.js('src/script.js', 'js')
+.sass('src/style.scss', 'css')
+.options({
+  processCssUrls: false
+})
+.vue();
+
+mix.disableNotifications();
